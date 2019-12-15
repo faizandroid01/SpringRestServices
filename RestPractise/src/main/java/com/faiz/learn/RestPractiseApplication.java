50c7abcdee5cf108e37b73e028b9961e2085b0d1
faiz-pitavya <faizrocks9211@gmail.com>
2019-12-15 18:09:54 +0530

    I18N  internationalization

--- RestPractise/src/main/java/com/faiz/learn/RestPractiseApplication.java
+++ RestPractise/src/main/java/com/faiz/learn/RestPractiseApplication.java
@@ -1,8 +1,14 @@
 package com.faiz.learn;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class RestPractiseApplication {
@@ -17,4 +23,18 @@ public class RestPractiseApplication {
 
 	}
 
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource bundleMessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("message");
+		return messageSource;
+	}
 }
--- RestPractise/src/main/java/com/faiz/learn/webcontrollers/MessageController.java
+++ RestPractise/src/main/java/com/faiz/learn/webcontrollers/MessageController.java
@@ -1,13 +1,21 @@
 package com.faiz.learn.webcontrollers;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.faiz.learn.model.Message;
 
 @RestController
 public class MessageController {
+
+	@Autowired
+	private MessageSource messageSource;
 
 	@GetMapping(path = "/message")
 	public Message getMessage() {
@@ -17,6 +25,14 @@ public class MessageController {
 	@GetMapping(path = "/message/{name}")
 	public Message getMessageWithName(@PathVariable String name) {
 		return new Message(String.format("Hello World : %s ", name));
+	}
+
+	@GetMapping(path = "/message-internationalized")
+	public String getMessageInternationalized(
+			@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+
+		return messageSource.getMessage("good.morning.message", null, locale);
+
 	}
 
 }
